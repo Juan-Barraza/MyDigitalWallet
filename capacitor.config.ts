d52819b4150e5c8ff4332d349068f32a378c8853
Juan Barraza <juanbarraza2004@outlook.com>
2026-04-14 21:55:09 -0500

    feat: add app icon and splash when you start the app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'My Digital Wallet',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#F5F7FA',
+      androidSplashResourceName: 'splash',
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+    }
+  }
 };
 
 export default config;
