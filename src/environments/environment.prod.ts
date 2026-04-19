3fdfcb3c3a9498b433bdc8bbef6f689a3437a14e
Juan Barraza <juanbarraza2004@outlook.com>
2026-04-18 20:13:40 -0500

    modific credentials to test the send message notification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBpMgRxEWx42BJipx7OuR7_GjgqKd8aW-g",
-    authDomain: "wallet-4a7e5.firebaseapp.com",
-    projectId: "wallet-4a7e5",
-    storageBucket: "wallet-4a7e5.firebasestorage.app",
-    messagingSenderId: "199278238464",
-    appId: "1:199278238464:web:0303d3f21272bd89c8b1e5"
+    apiKey: "AIzaSyBRoBJYhPxhnLfmrzdrJF6yyBPF6zoxfak",
+    authDomain: "mywallet-2bf24.firebaseapp.com",
+    projectId: "mywallet-2bf24",
+    storageBucket: "mywallet-2bf24.firebasestorage.app",
+    messagingSenderId: "538833798551",
+    appId: "1:538833798551:web:377a3091e315aa7ab930d2"
   },
-  clientIdWeb: '199278238464-a19dhulqonkdbj8kl32hnh2slh4ec6t4.apps.googleusercontent.com',
+  clientIdWeb: '538833798551-o5ou6hunohf4gnc1ne3k1gbicns7q05n.apps.googleusercontent.com',
   RAILWAY_URL: 'https://sendnotificationfirebase-production.up.railway.app',
   RAILWAY_EMAIL: 'juan.barrazapadilla@unicolombo.edu.co',
   RAILWAY_PASSWORD: '28Dejulio',
